7e1b0f34838d236063620557dc73cdfd1f5d2f10
TechnoSpiritMC <127853301+TechnoSpiritMC@users.noreply.github.com>
2023-03-17 20:04:13 +0100

    Update Extentions.py

--- Extentions.py
+++ Extentions.py
@@ -41,8 +41,6 @@ if TestEN:
     a1 = a1 + f"{c(100, 100, 100, 'Extention de test            : ')}{g('OUI')}\n"
 else:
     a1 = a1 + f"{c(100, 100, 100, 'Extention de test            : ')}{r('NON')}\n"
-    
-    
 
 TestEN = os.path.exists("AiScores.aiext")
 if TestEN:
@@ -52,8 +50,6 @@ if TestEN:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiScores           : ')}{g('OUI')}\n"
 else:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiScores           : ')}{r('NON')}\n"
-    
-    
 
 TestEN = os.path.exists("AiTrainer.aiext")
 if TestEN:
@@ -63,8 +59,6 @@ if TestEN:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiTrainer          : ')}{g('OUI')}\n"
 else:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiTrainer          : ')}{r('NON')}\n"
-    
-    
 
 TestEN = os.path.exists("AiGraphs.console.aiext")
 if TestEN:
@@ -74,8 +68,6 @@ if TestEN:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiGraphs.console   : ')}{g('OUI')}\n"
 else:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiGraphs.console   : ')}{r('NON')}\n"
-    
-    
 
 TestEN = os.path.exists("AiGraphs.turtle.aiext")
 if TestEN:
@@ -85,49 +77,59 @@ if TestEN:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiGraphs.turtle    : ')}{g('OUI')}\n"
 else:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiGraphs.turtle    : ')}{r('NON')}\n"
-    
-    
-    
 
 a1 = a1 + f"{c(100, 100, 100, f'Que voulez vous faire avec ces {a} extention(s)? Les')}{g('CHARGER (cha)')}{c(100, 100, 100, 'les')}{b('INSTALLER (ins)')}{c(100, 100, 100, 'ou les')}{r('IGNORER (ign)')}"
 a1 = a1 + "\n"
 
-
-
-
 chaINSign = input(a1)
 
 if chaINSign == "cha":
-    
+
     if a == 0:
         print(c(100, 100, 100, f"Tentative d'installation de {a} extentions disponnibles..."))
         time.sleep(1)
         print(f"{c(100, 100, 100, 'Tentative de chargement')}{r('infructueuse : aucune extention n est installée.')}")
-    else :
+    else:
         print(f'{c(100, 100, 100, f"Action en cours:")}{g(f"tentative de chargement des {a} extentions disponnibles")}')
+        i = 0
+        TimeBefore = time.time()
         TestEN = os.path.exists("test.aiext")
         if TestEN:
             with open("test.py", "w") as testWrite:
                 testWrite.write(testExtContent)
+                testC = 1
+
+
         TestEN = os.path.exists("AiScores.aiext")
         if TestEN:
             with open("AiScores.py", "w") as testWrite:
                 testWrite.write(AiScoresExtContent)
-        
+                AiScoresC = 1
+
+
         TestEN = os.path.exists("AiTrainer.aiext")
         if TestEN:
             with open("AiTrainer.py", "w") as testWrite:
                 testWrite.write(AiTrainerExtContent)
-        
+                AiTrainerC = 1
+
+
         TestEN = os.path.exists("AiGraphs.console.aiext")
         if TestEN:
             with open("AiGraphs.console.py", "w") as testWrite:
                 testWrite.write(AiGraphsCExtContent)
+                AiGraphsCC = 1
+
 
         TestEN = os.path.exists("AiGraphs.turtle.aiext")
         if TestEN:
             with open("AiGraphs.turtle.py", "w") as testWrite:
                 testWrite.write(AiGraphsTExtContent)
-                
-        print(f"{c(100, 100, 100, 'Opération effectuée avec')}{g('succès.')}")
+                AiGraphsTC = 1
 
+        TimeAfter = time.time()
+
+
+
+
+        print(f"{c(100, 100, 100, f'Opération effectuée en {round((TimeAfter-TimeBefore))} secondes avec')}{g('succès.')}")
