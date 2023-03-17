0f2ec0ace7412b21c48cadce833f0c19fd91550c
TechnoSpiritMC <127853301+TechnoSpiritMC@users.noreply.github.com>
2023-03-17 13:27:21 +0100

    Update Extentions.py

--- Extentions.py
+++ Extentions.py
@@ -1,4 +1,10 @@
-import os
+import os, time
+
+testExtContent = ""
+AiScoresExtContent = ""
+AiTrainerExtContent = ""
+AiGraphsCExtContent = ""
+AiGraphsTExtContent = ""
 
 
 def c(r, g, b, text):
@@ -27,44 +33,63 @@ a1 = f"{c(100, 100, 100, 'EXTENTIONS INSTALLÉES:')}"
 a1 = a1 + "\n"
 a = 0
 
-TestEN = os.path.exists("file.txt")
+TestEN = os.path.exists("test.aiext")
 if TestEN:
-    with open('file.txt', 'r') as file:
+    with open('test.aiext', 'r') as file:
         testExtContent = file.read()
     a = a + 1
     a1 = a1 + f"{c(100, 100, 100, 'Extention de test            : ')}{g('OUI')}\n"
 else:
     a1 = a1 + f"{c(100, 100, 100, 'Extention de test            : ')}{r('NON')}\n"
+    
+    
 
-TestEN = os.path.exists("AiScores.py")
+TestEN = os.path.exists("AiScores.aiext")
 if TestEN:
+    with open('AiScores.aiext', 'r') as file:
+        AiScoresExtContent = file.read()
     a = a + 1
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiScores           : ')}{g('OUI')}\n"
 else:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiScores           : ')}{r('NON')}\n"
+    
+    
 
-TestEN = os.path.exists("AiTrainer.py")
+TestEN = os.path.exists("AiTrainer.aiext")
 if TestEN:
+    with open('AiTrainer.aiext', 'r') as file:
+        AiTrainerExtContent = file.read()
     a = a + 1
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiTrainer          : ')}{g('OUI')}\n"
 else:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiTrainer          : ')}{r('NON')}\n"
+    
+    
 
-TestEN = os.path.exists("AiGraphs.console.py")
+TestEN = os.path.exists("AiGraphs.console.aiext")
 if TestEN:
+    with open('AiGraphs.console.aiext', 'r') as file:
+        AiGraphsCExtContent = file.read()
     a = a + 1
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiGraphs.console   : ')}{g('OUI')}\n"
 else:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiGraphs.console   : ')}{r('NON')}\n"
+    
+    
 
 TestEN = os.path.exists("AiGraphs.turtle.aiext")
 if TestEN:
+    with open('AiGraphs.turtle.aiext', 'r') as file:
+        AiGraphsTExtContent = file.read()
     a = a + 1
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiGraphs.turtle    : ')}{g('OUI')}\n"
 else:
     a1 = a1 + f"{c(100, 100, 100, 'Extention AiGraphs.turtle    : ')}{r('NON')}\n"
+    
+    
+    
 
-a1 = a1 + f"{c(100, 100, 100, f'Que voulez vous faire avec ces {a} extentions? Les')}{g('CHARGER (cha)')}{c(100, 100, 100, 'les')}{b('INSTALLER (ins)')}{c(100, 100, 100, 'ou les')}{r('IGNORER (ign)')}"
+a1 = a1 + f"{c(100, 100, 100, f'Que voulez vous faire avec ces {a} extention(s)? Les')}{g('CHARGER (cha)')}{c(100, 100, 100, 'les')}{b('INSTALLER (ins)')}{c(100, 100, 100, 'ou les')}{r('IGNORER (ign)')}"
 a1 = a1 + "\n"
 
 
@@ -73,10 +98,36 @@ a1 = a1 + "\n"
 chaINSign = input(a1)
 
 if chaINSign == "cha":
-    print(f'{c(100, 100, 100, f"Action en cours:")}{g(f"chargement des {a} extentions disponnibles")}')
-    with open("test.py", "w") as testWrite:
-        testWrite.write(testExtContent)
+    
+    if a == 0:
+        print(c(100, 100, 100, f"Tentative d'installation de {a} extentions disponnibles..."))
+        time.sleep(1)
+        print(f"{c(100, 100, 100, 'Tentative de chargement')}{r('infructueuse : aucune extention n est installée.')}")
+    else :
+        print(f'{c(100, 100, 100, f"Action en cours:")}{g(f"tentative de chargement des {a} extentions disponnibles")}')
+        TestEN = os.path.exists("test.aiext")
+        if TestEN:
+            with open("test.py", "w") as testWrite:
+                testWrite.write(testExtContent)
+        TestEN = os.path.exists("AiScores.aiext")
+        if TestEN:
+            with open("AiScores.py", "w") as testWrite:
+                testWrite.write(AiScoresExtContent)
+        
+        TestEN = os.path.exists("AiTrainer.aiext")
+        if TestEN:
+            with open("AiTrainer.py", "w") as testWrite:
+                testWrite.write(AiTrainerExtContent)
+        
+        TestEN = os.path.exists("AiGraphs.console.aiext")
+        if TestEN:
+            with open("AiGraphs.console.py", "w") as testWrite:
+                testWrite.write(AiGraphsCExtContent)
 
-
-
+        TestEN = os.path.exists("AiGraphs.turtle.aiext")
+        if TestEN:
+            with open("AiGraphs.turtle.py", "w") as testWrite:
+                testWrite.write(AiGraphsTExtContent)
+                
+        print(f"{c(100, 100, 100, 'Opération effectuée avec')}{g('succès.')}")
 
