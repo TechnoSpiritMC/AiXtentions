a528acdf2c9b11221979d4d39ed82b0d58c6d249
TechnoSpiritMC <127853301+TechnoSpiritMC@users.noreply.github.com>
2023-03-19 18:58:50 +0100

    Update mach1 de téléchargement.py

--- mach1 de téléchargement.py
+++ mach1 de téléchargement.py
@@ -1,7 +1,7 @@
 import requests
 
 
-url = 'truc'
+url = 'https://gist.githubusercontent.com/TechnoSpiritMC/a54b406837625abac62afee4b108aa18/raw/a70ecd72969dd4b1a149e86175a564da1b50d8e5/gistfile1.txt'
 r = requests.get(url, allow_redirects=True)
 
-open('facebook.txt', 'wb').write(r.content)
+open('AiScores.aiext', 'wb').write(r.content)
